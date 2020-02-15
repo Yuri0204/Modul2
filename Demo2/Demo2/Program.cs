4ef8262b4ae7552f6c321a36fc7664d235611022
Yuri <yuri_10@abv.bg>
2020-02-15 11:43:21 +0200

    cvbh

---
+++ Demo2/Demo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            if (X>10)
+            {
+                Console.WriteLine("Chisloto e dvucifreno");
+            }
+            else
+            {
+                Console.WriteLine("Other");
+            }
+        }
+    }
+}
