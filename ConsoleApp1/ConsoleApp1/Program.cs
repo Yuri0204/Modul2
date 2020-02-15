7c8cd355e405dc3d26e18752ce7cf116435ce775
Yuri <yuri_10@abv.bg>
2020-02-15 12:31:47 +0200

    wrh

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            double b = 5.00;
+            double c = 5;
+            decimal d = 5.00m;
+
+            Console.WriteLine(a);
+            Console.WriteLine($"{b}:f1");
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
