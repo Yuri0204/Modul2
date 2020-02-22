07b4b389b0e427d844d27f2e611bf8c790f64faf
Yuri <yuri_10@abv.bg>
2020-02-22 10:47:15 +0200

    rqrqweqa

---
+++ Solution1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine()); 
+        }
+    }
+}
