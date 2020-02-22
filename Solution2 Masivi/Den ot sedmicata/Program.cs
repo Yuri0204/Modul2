706b4f1f3b407ce8194bae1acc4a365e2db82384
Yuri <yuri_10@abv.bg>
2020-02-22 10:49:20 +0200

    Masivi

---
+++ Solution2 Masivi/Den ot sedmicata/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Den_ot_sedmicata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type in a day of the week as a number:");
+            int day = int.Parse(Console.ReadLine());
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if (day>=1 && day<=7)
+            {
+                Console.WriteLine(days[day-1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ Solution2 Masivi/Masiv 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masiv_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[5];
+            int[] num1 = new int[] { 3, 5, -6, 7, 8 };
+
+            //Console.WriteLine(num[0]);
+            // Console.WriteLine(num1[2]);
+            int[] num2 = { 1, 2, 3, 4, 5 };
+            // Console.WriteLine(num2[2]);
+
+            string[] days = { "Monday, Tuesday, Wednesday, Thursday,Friday"};
+            Console.WriteLine(days[3]);
+        }
+    }
+}
